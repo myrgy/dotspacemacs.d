3508c5ec8273c67a3de6d2a378d4d193224a36f6
Alexander Dalshov <dalshov@gmail.com>
2017-08-12 18:26:24 -0400

    fork rtags layer

---
+++ rtags/config.el
@@ -0,0 +1,11 @@
+;;; config.el --- rtags layer configuration file
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Prerequisites
+
+(configuration-layer/declare-layers '(auto-completion))
