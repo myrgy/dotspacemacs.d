aaaf22f490e222fbf06dcd754c6204561ffffcf3
Alexander Dalshov <dalshov@gmail.com>
2017-09-08 23:06:26 +0300

    enable plantuml

--- init.el
+++ init.el
@@ -56,6 +56,7 @@ This function should only modify configuration layer settings."
             c-c++-enable-rtags-support t
             c-c++-enable-clang-support t
             )
+     plantuml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
