a6e03060fdbd9e10a7e7fdb761f1665846428555
Alexander Dalshov <adalshov@charlesworthresearch.com>
2017-09-28 10:24:19 +0300

    add yalm, python modes; treat .h as c++

--- init.el
+++ init.el
@@ -29,7 +29,8 @@ This function should only modify configuration layer settings."
    dotspacemacs-configuration-layer-path '()
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(python
+   '(yaml
+     python
      ruby
      go
      graphviz
@@ -58,6 +59,7 @@ This function should only modify configuration layer settings."
      (c-c++ :variables
             c-c++-enable-rtags-support t
             c-c++-enable-clang-support t
+            c-c++-default-mode-for-headers 'c++-mode
             )
      plantuml
      )
