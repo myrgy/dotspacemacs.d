b5e614b0a84d45808b3df1bd1aa19982008cbdb2
Alexander Dalshov <dalshov@gmail.com>
2018-01-16 22:26:52 +0300

    add layer from my repo

---
+++ layers/my-code/funcs.el
@@ -0,0 +1,237 @@
+(defun my/ffap ()
+  (interactive)
+  (let ((filename (ffap-guess-file-name-at-point)))
+    (when (not filename)
+      (user-error "No file at point"))
+    (ffap filename)))
+
+
+;;; realgud
+
+(defun my/realgud-eval-nth-name-forward (n)
+  (interactive "p")
+  (save-excursion
+    (let (name)
+      (while (and (> n 0) (< (point) (point-max)))
+        (let ((p (point)))
+          (if (not (c-forward-name))
+              (progn
+                (c-forward-token-2)
+                (when (= (point) p) (forward-char 1)))
+            (setq name (buffer-substring-no-properties p (point)))
+            (cl-decf n 1))))
+      (when name
+        (realgud:cmd-eval name)
+        nil))))
+
+(defun my/realgud-eval-nth-name-backward (n)
+  (interactive "p")
+  (save-excursion
+    (let (name)
+      (while (and (> n 0) (> (point) (point-min)))
+        (let ((p (point)))
+          (c-backward-token-2)
+          (when (= (point) p) (backward-char 1))
+          (setq p (point))
+          (when (c-forward-name)
+            (setq name (buffer-substring-no-properties p (point)))
+            (goto-char p)
+            (cl-decf n 1))))
+      (when name
+        (realgud:cmd-eval name)
+        nil))))
+
+(defun my/realgud-eval-region-or-word-at-point ()
+  (interactive)
+  (when-let
+      ((cmdbuf (realgud-get-cmdbuf))
+       (process (get-buffer-process cmdbuf))
+       (expr
+        (if (evil-visual-state-p)
+            (let ((range (evil-visual-range)))
+              (buffer-substring-no-properties (evil-range-beginning range)
+                                              (evil-range-end range)))
+          (word-at-point)
+          )))
+    (with-current-buffer cmdbuf
+	    (setq realgud:process-filter-save (process-filter process))
+	    (set-process-filter process 'realgud:eval-process-output))
+    (realgud:cmd-eval expr)
+    ))
+
+
+;;; elisp
+
+(defun my/realtime-elisp-doc-function ()
+  (let ((w (selected-window)))
+    (when-let (s (intern-soft (current-word)))
+      (cond
+       ((fboundp s) (describe-function s))
+       ((boundp s) (describe-variable s))
+       )
+      (select-window w)
+      nil)))
+
+(defun my/realtime-elisp-doc ()
+  (interactive)
+  (when (eq major-mode 'emacs-lisp-mode)
+    (if (advice-function-member-p #'my/realtime-elisp-doc-function eldoc-documentation-function)
+        (remove-function (local 'eldoc-documentation-function) #'my/realtime-elisp-doc-function)
+      (add-function :after-while (local 'eldoc-documentation-function) #'my/realtime-elisp-doc-function))))
+
+
+;;; xref
+
+(defun my-xref/find-definitions ()
+  (interactive)
+  (if lsp-mode (lsp-ui-peek-find-definitions) (spacemacs/jump-to-definition)))
+
+(defun my-xref/find-references ()
+  (interactive)
+  (if lsp-mode (lsp-ui-peek-find-references) (spacemacs/jump-to-definition)))
+
+(defun my-xref//references-in-pair ()
+  (let ((refs (lsp--send-request (lsp--make-request
+                                  "textDocument/references"
+                                  (lsp--make-reference-params)))))
+    (sort
+     (mapcar
+      (lambda (ref)
+        (let* ((filename (string-remove-prefix lsp--uri-file-prefix (gethash "uri" ref)))
+               (range (gethash "range" ref))
+               (start (gethash "start" range))
+               (line (gethash "line" start))
+               (column (gethash "character" start)))
+          (list filename line column))) refs)
+     (lambda (x y)
+       (if (not (string= (car x) (car y)))
+           (string< (car x) (car y))
+         (if (not (= (cadr x) (cadr y)))
+             (< (cadr x) (cadr y))
+           (< (caddr x) (caddr y))))))))
+
+(defun my-xref/next-reference ()
+  (interactive)
+  (let* ((line (lsp--cur-line))
+         (column (lsp--cur-column))
+         (refs (my-xref//references-in-pair))
+         (res (-first (lambda (x)
+                        (if (not (string= (car x) buffer-file-name))
+                            (string> (car x) buffer-file-name)
+                          (if (not (= (cadr x) line))
+                              (> (cadr x) line)
+                            (> (caddr x) column)))) refs)))
+    (when res
+      (find-file (car res))
+      (goto-char 1)
+      (forward-line (cadr res))
+      (forward-char (caddr res))
+      nil)))
+
+(defun my-xref/previous-reference ()
+  (interactive)
+  (let* ((line (lsp--cur-line))
+         (column (lsp--cur-column))
+         (refs (my-xref//references-in-pair))
+         (res (-last (lambda (x)
+                        (if (not (string= (car x) buffer-file-name))
+                            (string< (car x) buffer-file-name)
+                          (if (not (= (cadr x) line))
+                              (< (cadr x) line)
+                            (< (caddr x) column)))) refs)))
+    (when res
+      (find-file (car res))
+      (goto-char 1)
+      (forward-line (cadr res))
+      (forward-char (caddr res))
+      nil)))
+
+;;; Override
+;; This function is transitively called by xref-find-{definitions,references,apropos}
+(require 'xref)
+(defun xref--show-xrefs (xrefs display-action &optional always-show-list)
+  (cond
+   ((cl-some (lambda (x) (string-match-p x buffer-file-name))
+             my-xref-blacklist)
+    nil)
+   ((and (not (cdr xrefs)) (not always-show-list))
+    ;; PATCH
+    (lsp-ui-peek--with-evil-jumps (evil-set-jump))
+
+    (xref--pop-to-location (car xrefs) display-action))
+   (t
+    ;; PATCH
+    (lsp-ui-peek--with-evil-jumps (evil-set-jump))
+
+    ;; PATCH Jump to the first candidate
+    ;; (when xrefs
+      ;; (xref--pop-to-location (car xrefs) display-action))
+
+    (funcall xref-show-xrefs-function xrefs
+             `((window . ,(selected-window)))))))
+
+
+;; https://github.com/syl20bnr/spacemacs/pull/9911
+
+(defmacro spacemacs|define-reference-handlers (mode &rest handlers)
+  "Defines reference handlers for the given MODE.
+This defines a variable `spacemacs-reference-handlers-MODE' to which
+handlers can be added, and a function added to MODE-hook which
+sets `spacemacs-reference-handlers' in buffers of that mode."
+  (let ((mode-hook (intern (format "%S-hook" mode)))
+        (func (intern (format "spacemacs//init-reference-handlers-%S" mode)))
+        (handlers-list (intern (format "spacemacs-reference-handlers-%S" mode))))
+    `(progn
+       (defvar ,handlers-list ',handlers
+         ,(format (concat "List of mode-specific reference handlers for %S. "
+                          "These take priority over those in "
+                          "`spacemacs-default-reference-handlers'.")
+                  mode))
+       (defun ,func ()
+         (setq spacemacs-reference-handlers
+               (append ,handlers-list
+                       spacemacs-default-reference-handlers)))
+       (add-hook ',mode-hook ',func)
+       (with-eval-after-load 'bind-map
+         (spacemacs/set-leader-keys-for-major-mode ',mode
+           "gr" 'spacemacs/jump-to-reference)))))
+
+(defun spacemacs/jump-to-reference ()
+  "Jump to reference around point using the best tool for this action."
+  (interactive)
+  (catch 'done
+    (let ((old-window (selected-window))
+          (old-buffer (current-buffer))
+          (old-point (point)))
+      (dolist (-handler spacemacs-reference-handlers)
+        (let ((handler (if (listp -handler) (car -handler) -handler))
+              (async (when (listp -handler)
+                       (plist-get (cdr -handler) :async))))
+          (ignore-errors
+            (call-interactively handler))
+          (when (or (eq async t)
+                    (and (fboundp async) (funcall async))
+                    (not (eq old-point (point)))
+                    (not (equal old-buffer (window-buffer old-window))))
+            (throw 'done t)))))
+    (message "No reference handler was able to find this symbol.")))
+
+(defun spacemacs/jump-to-reference-other-window ()
+  "Jump to reference around point in other window."
+  (interactive)
+  (let ((pos (point)))
+    ;; since `spacemacs/jump-to-reference' can be asynchronous we cannot use
+    ;; `save-excursion' here, so we have to bear with the jumpy behavior.
+    (switch-to-buffer-other-window (current-buffer))
+    (goto-char pos)
+    (spacemacs/jump-to-reference)))
+
+
+;; dumb-jump
+
+(defun my-advice/dumb-jump-go (orig-fun &rest args)
+  (unless (or lsp-mode
+              (cl-some
+               (lambda (x) (string-match-p x buffer-file-name))
+               my-xref-blacklist))
+    (apply orig-fun args)))
---
+++ layers/my-code/packages.el
@@ -0,0 +1,297 @@
+(defconst my-code-packages
+  '(
+    cc-mode
+    company-lsp
+    dumb-jump
+    evil
+    flycheck
+    haskell-mode
+    helm-xref
+    ;; (lsp-mode :location local)
+    lsp-mode
+    lsp-haskell
+    lsp-rust
+    ;;(lsp-ui :location local)
+    lsp-ui
+    markdown-mode
+    realgud
+    smartparens
+    ))
+
+(defun my-code/post-init-cc-mode ()
+  (dolist (mode c-c++-modes)
+    (spacemacs/declare-prefix-for-mode mode "mx" "format")
+    (spacemacs/set-leader-keys-for-major-mode mode
+      "xf" 'clang-format-region
+      "db" (lambda ()
+             (interactive)
+             (evil-open-above 1)
+             (insert "volatile static int z=0;while(!z)asm(\"pause\");")
+             (evil-normal-state)
+             )))
+  )
+
+(defun my-code/init-company-lsp ()
+  (use-package company-lsp
+    :after company
+    :init
+    (setq company-lsp-async t
+          company-lsp-cache-candidates nil)
+    (spacemacs|add-company-backends :backends company-lsp :modes c-mode-common)
+    ))
+
+(defun my-code/post-init-dumb-jump ()
+  ;; Don't use dumb-jump-go in large code base.
+  (advice-add 'dumb-jump-go :around #'my-advice/dumb-jump-go))
+
+(defun my-code/post-init-flycheck ()
+  (setq flycheck-check-syntax-automatically '(mode-enabled save)))
+
+(defun my-code/post-init-haskell-mode ()
+  (with-eval-after-load 'haskell-mode
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+    ;; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+    ;; (add-hook 'haskell-mode-hook 'lsp-mode)
+    ;; (intero-global-mode 1)
+    ;; (add-hook 'haskell-mode-hook 'intero-mode)
+    ;; (add-to-list 'spacemacs-jump-handlers-haskell-mode 'intero-goto-definition)
+    )
+  )
+
+(defun my-code/post-init-evil ()
+  (add-to-list 'evil-emacs-state-modes 'xref--xref-buffer-mode)
+
+  (define-key evil-normal-state-map "gf" 'my/ffap)
+  (define-key evil-normal-state-map (kbd "<backspace>") 'spacemacs/evil-search-clear-highlight)
+  (define-key evil-normal-state-map (kbd "C-p") 'lsp-ui-peek-jump-forward)
+  (define-key evil-normal-state-map (kbd "C-t") 'lsp-ui-peek-jump-backward)
+  (define-key evil-motion-state-map (kbd "M-?") 'xref-find-references)
+  (define-key evil-motion-state-map (kbd "C-,") #'my-xref/find-references)
+  ;; Also define M-, because C-, is unavailable in the terminal
+  (define-key evil-motion-state-map (kbd "M-,") #'my-xref/find-references)
+  (define-key evil-motion-state-map (kbd "C-j") #'my-xref/find-definitions)
+  (define-key evil-motion-state-map (kbd "M-n") 'next-error)
+  (define-key evil-motion-state-map (kbd "M-p") 'previous-error)
+  (define-key evil-normal-state-map (kbd "C-c P s") 'profiler-start)
+  (define-key evil-normal-state-map (kbd "C-c P r") 'profiler-report)
+  (define-key evil-normal-state-map (kbd "C-c P S") 'profiler-stop)
+
+  (spacemacs/set-leader-keys
+    "aa" (lambda ()
+           (interactive)
+           (let ((f (file-name-base (buffer-file-name))))
+             (set-buffer "*ansi-term-1*")
+             (term-send-raw-string (format "\C-umake %s && ./%s \C-m" f f))))
+    "ag" (lambda () (interactive) (shell-command-on-region (point-min) (point-max) "genhdr" t t))
+    "aG" (lambda () (interactive) (shell-command-on-region (point-min) (point-max) "genhdr windows" t t))
+    "TD" #'my/realtime-elisp-doc
+    ;; previous/next modified hunk in git
+    "gp" (lambda () (interactive) (git-gutter+-next-hunk -1))
+    "gn" (lambda () (interactive) (git-gutter+-next-hunk 1))
+    )
+
+  (add-hook 'TeX-mode-hook #'spacemacs/toggle-auto-fill-mode-off)
+  )
+
+(defun my-code/init-markdown-mode ()
+  (use-package markdown-mode))
+
+(defun my-code/init-helm-xref ()
+  (use-package helm-xref
+    :config
+    ;; This is required to make xref-find-references work in helm-mode.
+    ;; In helm-mode, it gives a prompt and asks the identifier (which has no text property) and then passes it to lsp-mode, which requires the text property at point to locate the references.
+    ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=29619
+    (setq xref-prompt-for-identifier '(not xref-find-definitions xref-find-definitions-other-window xref-find-definitions-other-frame xref-find-references spacemacs/jump-to-definition spacemacs/jump-to-reference))
+
+    ;; Use helm-xref to display xref.el results.
+    (setq xref-show-xrefs-function 'helm-xref-show-xrefs)
+    )
+  )
+
+(defun my-code/init-lsp-mode ()
+  (require 'lsp-imenu)
+  (use-package lsp-mode
+    :config
+    (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
+    (add-to-list 'spacemacs-jump-handlers-d-mode 'company-dcd-goto-definition)
+    (setq lsp-enable-flycheck nil) ;; disable lsp-flycheck.el in favor of lsp-ui-flycheck.el
+    ;; (setq-default flycheck-disabled-checkers '(c/c++-clang)) ;; in flycheck.el
+    )
+  )
+
+(defun my-code/init-lsp-ui ()
+  (use-package lsp-ui
+    :after lsp-mode
+    :after markdown-mode
+    :config
+    (setq lsp-ui-doc-include-signature nil)  ; don't include type signature in the child frame
+
+    ;; TODO slow https://github.com/emacs-lsp/lsp-ui/issues/45
+    ;; (lsp-ui-flycheck-enable 1)
+    (setq lsp-ui-flycheck-enable nil)
+    (setq lsp-ui-sideline-enable nil)
+
+    (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+
+    ;;; Override
+    (dolist (mode '("c" "c++" "go" "haskell" "javascript" "python" "rust"))
+      (let ((handler (intern (format "spacemacs-jump-handlers-%s-mode" mode))))
+        (add-to-list handler 'lsp-ui-peek-find-definitions))
+      (let ((handler (intern (format "spacemacs-reference-handlers-%s-mode" mode))))
+        (add-to-list handler 'lsp-ui-peek-find-references)))
+
+    (setq lsp-ui-peek-expand-function (lambda (xs) (mapcar #'car xs)))
+    (evil-make-overriding-map lsp-ui-peek-mode-map 'normal)
+    (define-key lsp-ui-peek-mode-map (kbd "h") 'lsp-ui-peek--select-prev-file)
+    (define-key lsp-ui-peek-mode-map (kbd "l") 'lsp-ui-peek--select-next-file)
+    (define-key lsp-ui-peek-mode-map (kbd "j") 'lsp-ui-peek--select-next)
+    (define-key lsp-ui-peek-mode-map (kbd "k") 'lsp-ui-peek--select-prev)
+
+    (setq lsp-ui-sideline-show-symbol nil)  ; don't show symbol on the right of info
+    (setq lsp-ui-sideline-ignore-duplicate t)
+    (set-face-attribute 'lsp-ui-sideline-symbol nil :foreground "grey30" :box nil)
+    (set-face-attribute 'lsp-ui-sideline-current-symbol nil :foreground "grey38" :box nil)
+    ;; (when (internal-lisp-face-p 'lsp-ui-sideline-contents)
+    ;;   (set-face-attribute 'lsp-ui-sideline-contents nil :foreground "grey35")
+    ;;   (set-face-attribute 'lsp-ui-sideline-current-contents nil :foreground "grey43"))
+
+    (defun my-cquery/base () (interactive) (lsp-ui-peek-find-custom 'base "$cquery/base"))
+    (defun my-cquery/callers () (interactive) (lsp-ui-peek-find-custom 'callers "$cquery/callers"))
+    (defun my-cquery/derived () (interactive) (lsp-ui-peek-find-custom 'derived "$cquery/derived"))
+    (defun my-cquery/vars () (interactive) (lsp-ui-peek-find-custom 'vars "$cquery/vars"))
+
+    (dolist (mode c-c++-modes)
+      (spacemacs/set-leader-keys-for-major-mode mode
+        "la" #'lsp-ui-find-workspace-symbol
+        "lA" #'lsp-ui-peek-find-workspace-symbol
+        "lb" #'my-cquery/base
+        "lc" #'my-cquery/callers
+        "ld" #'my-cquery/derived
+        ;; Formatting requires waf configure --use-clang-cxx
+        ;; https://github.com/jacobdufault/cquery/wiki/Formatting
+        "lf" #'lsp-format-buffer
+        "ll" #'lsp-ui-sideline-mode
+        "lD" #'lsp-ui-doc-mode
+        "ln" #'my-xref/next-reference
+        "lp" #'my-xref/previous-reference
+        "lr" #'lsp-rename
+        "lv" #'my-cquery/vars
+       ))
+
+    (define-key evil-motion-state-map (kbd "M-<down>") 'my-xref/next-reference)
+    (define-key evil-motion-state-map (kbd "M-<up>") 'my-xref/previous-reference)
+    ))
+
+(defun my-code/init-lsp-haskell ()
+  (use-package lsp-haskell
+    :mode ("\\.hs\\'" . haskell-mode)
+    :after lsp-mode
+    :config
+    )
+  )
+
+(defun my-code/init-lsp-rust ()
+  (use-package lsp-rust
+    :defer t
+    :after lsp-mode
+    :config
+    (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls"))
+    (add-hook 'rust-mode-hook #'lsp-rust-enable)))
+
+(defun my-code/post-init-realgud ()
+  (with-eval-after-load 'realgud
+    ;; It was bound to [mouse-2], but [mouse-1] is more convenient.
+    (define-key realgud:shortkey-mode-map [mouse-1] #'realgud:tooltip-eval)
+    (define-key realgud:shortkey-mode-map (kbd "p") #'my/realgud-eval-region-or-word-at-point)
+    (define-key realgud:shortkey-mode-map (kbd "C-o") #'evil-execute-in-normal-state)
+    (define-key realgud:shortkey-mode-map (kbd "w") #'evil-forward-word-begin)
+    (define-key realgud:shortkey-mode-map (kbd "W") #'evil-forward-WORD-begin)
+
+    ;; Don't use default bindings in c-c++/packages.el
+    (evilified-state-evilify-map realgud:shortkey-mode-map
+      :eval-after-load realgud
+      :mode realgud-short-key-mode
+      :bindings
+      "C-j" 'lsp-ui-peek-find-definitions
+      "C-," 'lsp-ui-peek-find-references
+      "C-t" 'lsp-ui-peek-jump-backward
+      "C-p" 'lsp-ui-peek-jump-forward
+      "n" 'realgud:cmd-next
+      "s" 'realgud:cmd-step
+      "b" 'realgud:cmd-break
+      "B" 'realgud:cmd-clear
+      "f" 'realgud:cmd-finish
+      "c" 'realgud:cmd-continue
+      "e" 'realgud:cmd-eval
+      "r" 'realgud:cmd-restart
+      "q" 'realgud:cmd-quit
+      "S" 'realgud-window-cmd-undisturb-src)
+
+    ;; Rebind 1 .. 9 to M-1 .. M-9
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-1") #'realgud-goto-arrow1)
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-2") #'realgud-goto-arrow2)
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-3") #'realgud-goto-arrow3)
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-4") #'realgud:goto-loc-hist-4)
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-5") #'realgud:goto-loc-hist-5)
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-6") #'realgud:goto-loc-hist-6)
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-7") #'realgud:goto-loc-hist-7)
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-8") #'realgud:goto-loc-hist-8)
+    ;; (define-key realgud:shortkey-mode-map (kbd "M-9") #'realgud:goto-loc-hist-9)
+
+    (define-key realgud:shortkey-mode-map (kbd "1") (lambda () (interactive) (my/realgud-eval-nth-name-forward 1)))
+    (define-key realgud:shortkey-mode-map (kbd "2") (lambda () (interactive) (my/realgud-eval-nth-name-forward 2)))
+    (define-key realgud:shortkey-mode-map (kbd "3") (lambda () (interactive) (my/realgud-eval-nth-name-forward 3)))
+    (define-key realgud:shortkey-mode-map (kbd "4") (lambda () (interactive) (my/realgud-eval-nth-name-forward 4)))
+    (define-key realgud:shortkey-mode-map (kbd "5") (lambda () (interactive) (my/realgud-eval-nth-name-forward 5)))
+    (define-key realgud:shortkey-mode-map (kbd "6") (lambda () (interactive) (my/realgud-eval-nth-name-forward 6)))
+    (define-key realgud:shortkey-mode-map (kbd "7") (lambda () (interactive) (my/realgud-eval-nth-name-forward 7)))
+    (define-key realgud:shortkey-mode-map (kbd "8") (lambda () (interactive) (my/realgud-eval-nth-name-forward 8)))
+    (define-key realgud:shortkey-mode-map (kbd "9") (lambda () (interactive) (my/realgud-eval-nth-name-forward 9)))
+
+    (define-key realgud:shortkey-mode-map (kbd "M-1") (lambda () (interactive) (my/realgud-eval-nth-name-backward 1)))
+    (define-key realgud:shortkey-mode-map (kbd "M-2") (lambda () (interactive) (my/realgud-eval-nth-name-backward 2)))
+    (define-key realgud:shortkey-mode-map (kbd "M-3") (lambda () (interactive) (my/realgud-eval-nth-name-backward 3)))
+    (define-key realgud:shortkey-mode-map (kbd "M-4") (lambda () (interactive) (my/realgud-eval-nth-name-backward 4)))
+    (define-key realgud:shortkey-mode-map (kbd "M-5") (lambda () (interactive) (my/realgud-eval-nth-name-backward 5)))
+    (define-key realgud:shortkey-mode-map (kbd "M-6") (lambda () (interactive) (my/realgud-eval-nth-name-backward 6)))
+    (define-key realgud:shortkey-mode-map (kbd "M-7") (lambda () (interactive) (my/realgud-eval-nth-name-backward 7)))
+    (define-key realgud:shortkey-mode-map (kbd "M-8") (lambda () (interactive) (my/realgud-eval-nth-name-backward 8)))
+    (define-key realgud:shortkey-mode-map (kbd "M-9") (lambda () (interactive) (my/realgud-eval-nth-name-backward 9)))
+    ))
+
+(defun my-code/post-init-smartparens ()
+  (with-eval-after-load 'smartparens
+    (define-key smartparens-mode-map (kbd "C-M-f") 'sp-forward-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-b") 'sp-backward-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-d") 'sp-down-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-a") 'sp-backward-down-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-e") 'sp-up-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-u") 'sp-backward-up-sexp)
+    (define-key smartparens-mode-map (kbd "M-t") 'sp-transpose-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-n") 'sp-next-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-p") 'sp-previous-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-k") 'sp-kill-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-w") 'sp-copy-sexp)
+    (define-key smartparens-mode-map (kbd "C-S-d") 'sp-beginning-of-sexp)
+    (define-key smartparens-mode-map (kbd "C-S-a") 'sp-end-of-sexp)
+    (define-key smartparens-mode-map (kbd "C-S-f") 'sp-forward-symbol)
+    (define-key smartparens-mode-map (kbd "C-S-b") 'sp-backward-symbol)
+    (define-key smartparens-mode-map (kbd "M-k") 'sp-backward-kill-sexp)
+    (define-key smartparens-mode-map (kbd "M-]") 'sp-unwrap-sexp)
+    (define-key smartparens-mode-map (kbd "M-[") 'sp-backward-unwrap-sexp)
+    (define-key smartparens-mode-map (kbd "M-<delete>") 'sp-unwrap-sexp)
+    (define-key smartparens-mode-map (kbd "M-<backspace>") 'sp-backward-unwrap-sexp)
+    (define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
+    (define-key smartparens-mode-map (kbd "C-<left>") 'sp-forward-barf-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
+    (define-key smartparens-mode-map (kbd "C-M-<right>") 'sp-backward-barf-sexp)
+    (define-key smartparens-mode-map (kbd "C-c \"") (lambda () (interactive) (sp-wrap-with-pair "\"")))
+    (define-key smartparens-mode-map (kbd "C-c '") (lambda () (interactive) (sp-wrap-with-pair "'")))
+    (define-key smartparens-mode-map (kbd "C-c (") (lambda () (interactive) (sp-wrap-with-pair "(")))
+    (define-key smartparens-mode-map (kbd "C-c [") (lambda () (interactive) (sp-wrap-with-pair "[")))
+    (define-key smartparens-mode-map (kbd "C-c {") (lambda () (interactive) (sp-wrap-with-pair "{")))
+    )
+  )
+
+;;; packages.el ends here
