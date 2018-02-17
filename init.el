62f0f46987e52deb349339b16d760216c75af488
Alexander Dalshov <adalshov@unknown58b03561cec7.attlocal.net>
2018-02-16 19:34:49 -0600

    disable vim-extra - folding doesnt work propertly

--- init.el
+++ init.el
@@ -86,7 +86,7 @@ This function should only modify configuration layer settings."
      treemacs
      sr-speedbar
      ycmd
-     vim-extra
+     ;; vim-extra
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
