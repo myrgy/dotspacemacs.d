c93787495b02efc5909a6ee7e4dce7ca70df6e99
Alexander Dalshov <adalshov@charlesworthresearch.com>
2018-06-06 11:05:04 +0300

    fix pdf layer name

--- init.el
+++ init.el
@@ -42,7 +42,7 @@ This function should only modify configuration layer settings."
      rust
      go
      graphviz
-     pdf-tools
+     pdf
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
